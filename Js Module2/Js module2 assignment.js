6f442f94a0d1ef40e11fcde24dd891a6f1acdc6c
Salma Aktar Bithy <76905965+BithySalma@users.noreply.github.com>
2023-08-06 18:17:36 +0600

    updated

---
+++ Js Module2/Js module2 assignment.js
@@ -0,0 +1,5 @@
+//Assignment no:1
+
+function changeImage(){
+    document.getElementById("image").innerHTML='<img src="https://static.vecteezy.com/system/resources/previews/003/330/826/non_2x/bangladesh-map-silhouette-with-flag-on-black-background-free-vector.jpg" width="500" height="500" alt="New Image">';
+   } 
